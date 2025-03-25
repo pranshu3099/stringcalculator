cdf07d39f332f20e86717bfd57ac62635d2d2cbd
pranshu <srivastavapranshu2@gmail.com>
2025-03-25 18:42:48 +0530

    feat: support comma and newline as delimiters

--- index.test.ts
+++ index.test.ts
@@ -24,4 +24,7 @@ describe("verfifies Add function is working accordingly", () => {
   test("an invalid number 'f' => '1,2,f' throws invalid input", () => {
     expect(() => Add("1,2,f")).toThrowError(Error("invalid input"));
   });
+  test("comma and newline delimiters => '1,2\\n3' returns 6", () => {
+    expect(Add("1,2\n3")).toBe(6);
+  });
 });
--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ export function greet() {
 export function Add(input: string) {
   let sum = 0;
   if (input === "") return sum;
-  input.split(",").forEach((num) => {
+  const delimiter = new RegExp(/[,\n]/);
+  input.split(delimiter).forEach((num) => {
     const number = Number(num);
     if (isNaN(number)) {
       throw Error("invalid input");
