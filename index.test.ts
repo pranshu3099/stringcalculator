0d7cdc19c44126971f80ccb1777a89f8c2e9071c
pranshu <srivastavapranshu2@gmail.com>
2025-03-25 18:32:37 +0530

    feat: create add function, verify base case

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,16 @@
-import { greet } from ".";
+import { greet, Add } from ".";
 
 describe("checks if project setup is working", () => {
   test("greet function is compiled without errors", () => {
     expect(greet()).toBe("hello");
+  });
+});
+
+describe("verfifies Add function is working accordingly", () => {
+  test("Add is a valid function exported properly", () => {
+    expect(typeof Add).toBe("function");
+  });
+  test("empty string returns zero", () => {
+    expect(Add("")).toBe(0);
   });
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 export function greet() {
   return "hello";
 }
+export function Add(input: string) {
+  let sum = 0;
+  if (input === "") return sum;
+  return sum;
+}
